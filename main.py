61fee0f377101623a03dbfda18548997b069f421
popeyeschickentenders <70531289+popeyeschickentenders@users.noreply.github.com>
2023-06-12 21:28:29 +0800

    Update main.py
    finished storage layout, changed parameters to only width and row
    added position_index

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import PySimpleGUI as gui
 import time as t
 import keyboard as kb
 
+
+
 def storage_layout():
     # this creates the layout of the window
     layout = [[gui.Text("PLEASE INPUT ALL MEASUREMENTS AS CM")],
               [gui.Text("Enter WIDTH of compartment: "), gui.InputText(key='width')],
               # the key is to make it easier to know what values i am retrieving later
-              [gui.Text("Enter HEIGHT of compartment: "), gui.InputText(key='height')],
-              [gui.Text("Enter DEPTH of compartment: "), gui.InputText(key='depth')],
+              [gui.Text("Enter number of ROWS of compartment: "), gui.InputText(key='rows')],
               [gui.Button("Enter"), gui.Button("Cancel")]
               ]
 
@@ -35,28 +36,54 @@ def storage_input():
         window = storage_layout()
 
         event, values = window.read()
-        flt_width = float(values["width"])
-        flt_height = float(values["height"])
-        flt_depth = float(values["depth"])
 
         if event == gui.WIN_CLOSED or event == "Cancel": # if user closes window or clicks cancel
             break
 
         if event == "Enter":
             try: #condition to see if user input is a float
+                flt_width = float(values["width"])
+                flt_rows = float(values["rows"])
 
-                return {"width" : flt_width, "height" : flt_height, "depth" : flt_depth}
+                storage_dict = {"width" : flt_width, "rows" : flt_rows}
+
+                confirm_layout = [[gui.Text(f"You entered:")],
+                                  [gui.Text(storage_dict)],
+                                  [gui.Button("Confirm"), gui.Button("Cancel")]
+                                  ]
+
+                confirm_window = gui.Window("Confirm input ", confirm_layout)
+
+                confirm_event, confirm_values = confirm_window.read()
+
+                if confirm_event == gui.WIN_CLOSED or confirm_event == 'Cancel':
+                    pass
+                if confirm_event == 'Confirm':
+                    cd_round = storage_dict['width']//1
+                    cd_num = cd_round * storage_dict['rows']
+                    return cd_num
+                    pass
                 break
+
+
+
             except ValueError as v_error:
                 error_layout = [[gui.Text(f"One of your inputs is not valid")],
                                 [gui.Text(f"Error: {v_error}")],
-                                [gui.Text(f"Please re-enter valid inputs")]]
+                                [gui.Text(f"Please re-enter valid inputs")],
+                                [gui.Button("Retry"), gui.Button("Cancel")]
+                                ]
 
-                error_window = gui.Window("Error Occured...", error_layout)
+                error_window = gui.Window("Error Occurred...", error_layout)
+
                 window.close()
-                error_window.read()
 
-                t.sleep(4)
+                error_event, error_values = error_window.read()
+
+                if error_event == gui.WIN_CLOSED or error_event == "Cancel":
+                    break
+                if error_event == "Enter":
+                    pass
                 error_window.close()
                 pass
 
@@ -67,10 +94,13 @@ def storage_input():
 
     window.close()
 
+def position_index(cd_num):
+    for i in range(0, int(cd_num)):
+        pass
 
 
+print(storage_input())
 
-gui.popup(storage_input())
 '''def pop_up():
     gui.popup("hello world")
 
